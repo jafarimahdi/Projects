05cf77f7de7d7e0cd588788d8238fb034f281c16
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-07-12 20:26:29 +0200

    Add Register & Login Form App to C# folder

---
+++ C#/RagisterAndLogin-form/RagisterAndLogin-form/Controllers/loginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RagisterAndLogin_form.Models;
+
+namespace RagisterAndLogin_form.Controllers
+{
+    public class loginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if(userModel.UserName =="BillGates" & userModel.Password == "bigBucks") 
+            {
+            return View("LoginSuccess", userModel);
+
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+             
+        }
+    }
+}
---
+++ C#/RagisterAndLogin-form/RagisterAndLogin-form/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RagisterAndLogin_form.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
