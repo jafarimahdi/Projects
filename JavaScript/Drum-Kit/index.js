35eca905f420b5f7d6ca50849a6bb8aeb757bc83
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-09-30 23:57:45 +0200

    Drum

---
+++ JavaScript/Drum-Kit/index.js
@@ -0,0 +1,63 @@
+// detecting Button Press
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document
+        .querySelectorAll(".drum")[i].addEventListener("click", function() {
+            let buttoninnerHTML = this.innerHTML;
+            makeSound(buttoninnerHTML);
+            buttonAnimation(buttoninnerHTML);
+        });
+}
+// detection keyboard Press
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key)
+    buttonAnimation(event.key);
+});
+
+// method to play sound 
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick_bass = new Audio("./sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        case "l":
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        default:
+            console.log(buttoninnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    let activButton = document.querySelector("." + currentKey);
+    activButton.classList.add("pressed");
+    setTimeout(function() {
+        activButton.classList.remove("pressed");
+    }, 100)
+}
