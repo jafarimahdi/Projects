24b211398bab8e4825f56bd72d00565d27f84482
jafarimahdi <mahdijafarii.mj@gmail.com>
2023-10-07 15:15:36 +0200

    Silly_name

---
+++ Node.js/silly-name/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname"
+import superheroes from 'superheroes'
+
+// var generateName = require('sillyname');
+let sillyName = generateName();
+console.log(`Your Favorite name can be: ${sillyName}`);
+
+// ----------------superheroes.all;
+superheroes.all;
+let hero = superheroes.random();
+console.log(`Our today hero's is: ${hero}`);
